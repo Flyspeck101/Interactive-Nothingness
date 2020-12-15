878d5e92d2501f20a52cdd78f1beb1fc3b340dd2
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2020-12-15 14:24:13 +1100

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,12 @@
 (function(){if(document.getElementById()){alert("Your browser does not support JavaScript.")}})()
-
+let content = false
 function makePage() {
-  document.getElementById("main").innerHTML = "You clicked the button. You found the bottom of the page. *fart noises* oh, you farted."
-  document.getElementById("title").innerHTML = "ree"
+  if (content) {
+    location.reload()
+  } else {
+    document.getElementById("main").innerHTML = "You clicked the button. You found the bottom of the page. *fart noises* oh, you farted."
+    document.getElementById("title").innerHTML = "ree"
+    document.getElementById("button").innerHTML = "Reload"
+    content = true
+  }
 }
